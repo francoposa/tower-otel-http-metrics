d122d71cafa159f6b5635e86ac194683bf2bbcea
francoposa <franco@francoposa.io>
2022-07-19 11:46:18 -0700

    basic request handling and parsing

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,16 +1,25 @@
 use axum::{
+    body::Body,
+    http::{header::HeaderMap, Request},
+    response::IntoResponse,
     routing::get,
     Router,
 };
 
 #[tokio::main]
 async fn main() {
-    // build our application with a single route
-    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+    let app = Router::new().route("/", get(hello));
 
-    // run it with hyper on localhost:3000
-    axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
+    axum::Server::bind(&"127.0.0.1:3000".parse().unwrap())
         .serve(app.into_make_service())
         .await
         .unwrap();
+}
+
+async fn hello(request: Request<Body>) -> impl IntoResponse {
+    let x = request.headers();
+    let y = request.body();
+    println!("{:?}", x);
+    println!("{:?}", y);
+    "hello, world"
 }
