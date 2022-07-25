c9634194ba1048ae5afcd8bf213985c33b86855b
francoposa <franco@francoposa.io>
2022-07-25 11:30:08 -0700

    using request parts api to split headers and body; basic usage of hashmap

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,14 +1,16 @@
+use std::collections::HashMap;
 use axum::{
     body::Body,
-    http::{header::HeaderMap, Request},
-    response::IntoResponse,
+    http::{Request},
+    response::Json,
     routing::get,
     Router,
 };
+use serde_json::{Value, json};
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/", get(hello));
+    let app = Router::new().route("/", get(echo));
 
     axum::Server::bind(&"127.0.0.1:3000".parse().unwrap())
         .serve(app.into_make_service())
@@ -16,10 +18,20 @@ async fn main() {
         .unwrap();
 }
 
-async fn hello(request: Request<Body>) -> impl IntoResponse {
-    let x = request.headers();
-    let y = request.body();
-    println!("{:?}", x);
-    println!("{:?}", y);
-    "hello, world"
+async fn echo(request: Request<Body>) -> Json<Value> {
+    let (req_parts, req_body) = request.into_parts();
+    let req_method = req_parts.method.as_str();
+    let req_headers = req_parts.headers;
+
+    // let req_host = match req_headers.get("Host") {
+    //     None => "",
+    //     Some(method) => method.to_str().unwrap(),
+    // };
+
+    // let mut resp_body = HashMap::new();
+    // resp_body.insert("method".to_string(), req_method);
+
+    println!("{:?}", req_headers);
+    println!("{:?}", req_body);
+    Json(json!({}))
 }
