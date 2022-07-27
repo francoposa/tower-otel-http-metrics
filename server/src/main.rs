a050d1c71572a40106fbe2024fc18dc30949d9dd
francoposa <franco@francoposa.io>
2022-07-27 10:03:54 -0700

    remove pretty json, autoformat

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,22 +1,19 @@
 use std::collections::HashMap;
-use axum::{
-    body::Body,
-    http::{Request},
-    routing::get,
-    Router,
-};
-use axum_extra::response::ErasedJson;
+
+use axum::{body::Body, http::Request, Json, Router, routing::get};
 use serde::Serialize;
+use serde_json::{json, Value};
 
 #[tokio::main]
 async fn main() {
     let app = Router::new().route("/", get(echo));
 
-    axum::Server::bind(&"127.0.0.1:3000".parse().unwrap())
+    axum::Server::bind(&"127.0.0.1:8080".parse().unwrap())
         .serve(app.into_make_service())
         .await
         .unwrap();
 }
+
 #[derive(Serialize, Debug)]
 struct EchoResponse {
     method: String,
@@ -24,7 +21,7 @@ struct EchoResponse {
     body: String,
 }
 
-async fn echo(request: Request<Body>) -> ErasedJson {
+async fn echo(request: Request<Body>) -> Json<Value> {
     let (req_parts, req_body) = request.into_parts();
 
     let req_method = req_parts.method.to_string();
@@ -33,7 +30,7 @@ async fn echo(request: Request<Body>) -> ErasedJson {
         |(k, v)| (
             k.to_string(), v.to_str().unwrap_or_default().to_string()
         )
-    ).collect::<HashMap<String,String>>();
+    ).collect::<HashMap<String, String>>();
 
     let parsed_req_body = match hyper::body::to_bytes(req_body).await {
         Ok(bytes) => match String::from_utf8(bytes.to_vec()) {
@@ -43,11 +40,11 @@ async fn echo(request: Request<Body>) -> ErasedJson {
         Err(_) => String::new(),
     };
 
-    let resp_body = EchoResponse{
+    let resp_body = EchoResponse {
         method: req_method,
         headers: parsed_req_headers,
         body: parsed_req_body,
     };
 
-    ErasedJson::pretty(resp_body)
+    Json(json!(resp_body))
 }
