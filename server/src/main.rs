5c3cb370d685a74168d2720a93acb8bcdfb4f60d
francoposa <franco@francoposa.io>
2023-06-17 19:44:01 -0700

    convert metrics to opentelemetry libraries

--- server/src/main.rs
+++ server/src/main.rs
@@ -10,7 +10,9 @@ use hyper::HeaderMap;
 use hyper::server::Server;
 use metrics_exporter_prometheus::PrometheusBuilder;
 use opentelemetry::{global, KeyValue};
+use opentelemetry::sdk::export::metrics::aggregation::stateless_temporality_selector;
 use opentelemetry::sdk::export::trace::stdout as opentelemetry_stdout;
+use opentelemetry::sdk::metrics::selectors::simple::inexpensive;
 use opentelemetry::sdk::Resource;
 use opentelemetry_otlp::{self, WithExportConfig};
 use serde::Serialize;
@@ -24,11 +26,6 @@ use tracing_subscriber::{prelude::*, Registry};
 
 const SERVICE_NAME: &str = "echo-server";
 
-const REQUEST_COUNT_METRIC_NAME: &str = "request_count";
-const METRIC_LABEL_SERVICE: &str = "service";
-const METRIC_LABEL_REQUEST_ENDPOINT: &str = "endpoint";
-const METRIC_LABEL_REQUEST_METHOD: &str = "method";
-
 #[tokio::main]
 async fn main() {
     // file writer layer to collect all levels of logs, mostly useful for debugging the logging setup
@@ -37,6 +34,11 @@ async fn main() {
     let file_writer_layer = tracing_subscriber::fmt::layer()
         .json()
         .with_writer(file_writer);
+
+    // stdout/stderr log layer for non-tracing logs to be collected into ElasticSearch or similar
+    let std_stream_bunyan_format_layer =
+        BunyanFormattingLayer::new(SERVICE_NAME.into(), std::io::stderr)
+            .with_filter(LevelFilter::INFO);
 
     //
     // opentelemetry-formatted tracing layer to send traces to collector
@@ -49,8 +51,17 @@ async fn main() {
     // use this stdout pipeline instead to debug or view the opentelemetry data without a collector
     // let otel_tracer = opentelemetry_stdout::new_pipeline().install_simple();
 
+    let otlp_resource = Resource::new(
+        vec![
+            KeyValue::new(
+                opentelemetry_semantic_conventions::resource::SERVICE_NAME,
+                SERVICE_NAME,
+            )
+        ]
+    );
+
     // this pipeline will log connection errors to stderr if it cannot reach the collector endpoint
-    let otel_pipeline = opentelemetry_otlp::new_pipeline()
+    let otel_trace_pipeline = opentelemetry_otlp::new_pipeline()
         .tracing()
         .with_exporter(
             opentelemetry_otlp::new_exporter()
@@ -59,49 +70,43 @@ async fn main() {
         )
         .with_trace_config(
             opentelemetry::sdk::trace::config().with_resource(
-                Resource::new(
-                    vec![
-                        KeyValue::new(
-                            opentelemetry_semantic_conventions::resource::SERVICE_NAME,
-                            SERVICE_NAME,
-                        )
-                    ]
-                )
+                otlp_resource.clone(),
             ),
         );
-    let otel_tracer = otel_pipeline
+    let otel_tracer = otel_trace_pipeline
         .install_batch(opentelemetry::runtime::Tokio)
         .unwrap();
-    let otel_layer = tracing_opentelemetry::layer().with_tracer(otel_tracer);
+    let otel_log_trace_layer = tracing_opentelemetry::layer().with_tracer(otel_tracer);
 
-    // stdout/stderr log layer for non-tracing logs to be collected into ElasticSearch or similar
-    let std_stream_bunyan_format_layer =
-        BunyanFormattingLayer::new(SERVICE_NAME.into(), std::io::stderr)
-            .with_filter(LevelFilter::INFO);
+    // let otel_metrics_controller = ;
+    let otel_metrics_layer = tracing_opentelemetry::MetricsLayer::new(opentelemetry_otlp::new_pipeline()
+        .metrics(
+            inexpensive(),
+            stateless_temporality_selector(),
+            opentelemetry::runtime::Tokio,
+        )
+        .with_exporter(
+            opentelemetry_otlp::new_exporter()
+                .tonic()
+                .with_endpoint("http://localhost:4317"),
+        )
+        .with_resource(
+            otlp_resource.clone(),
+        )
+        .build()
+        .unwrap()
+    );
 
     let subscriber = Registry::default()
         .with(file_writer_layer)
         .with(JsonStorageLayer) // stores fields across spans for the bunyan formatter
         .with(std_stream_bunyan_format_layer)
-        .with(otel_layer);
+        .with(otel_log_trace_layer)
+        .with(otel_metrics_layer);
 
     tracing::subscriber::set_global_default(subscriber).unwrap();
 
-    // let meter = opentelemetry_otlp::new_pipeline()
-    //     .metrics(
-    //         inexpensive(),
-    //         stateless_temporality_selector(),
-    //         opentelemetry::runtime::Tokio,
-    //     )
-    //     .with_exporter(
-    //         opentelemetry_otlp::new_exporter()
-    //             .tonic()
-    //             .with_endpoint("http://localhost:4317"),
-    //     )
-    //     .build()
-    //     .unwrap();
-
-    PrometheusBuilder::new().install().unwrap();
+    // PrometheusBuilder::new().install().unwrap();
 
     let app = Router::new()
         .route("/", get(echo))
@@ -123,24 +128,25 @@ async fn main() {
         .unwrap();
 }
 
-#[instrument(skip(headers, bytes), fields(req.body.len = bytes.len()))]
+#[instrument(skip(headers, bytes))]
 pub async fn echo(
     matched_path: MatchedPath,
     method: Method,
     headers: HeaderMap,
     bytes: Bytes,
 ) -> Bytes {
-    metrics::increment_counter!(
-        String::from(REQUEST_COUNT_METRIC_NAME),
-        METRIC_LABEL_SERVICE => SERVICE_NAME,
-        METRIC_LABEL_REQUEST_ENDPOINT => String::from(matched_path.as_str()),
-        METRIC_LABEL_REQUEST_METHOD => String::from(method.as_str()),
+    // ideally this would be in some middleware instead of manually in each handler
+    info!(
+        monotonic_counter.request_count = 1,
+        request.endpoint = String::from(matched_path.as_str()),
+        request.method = String::from(method.as_str()),
     );
     let parsed_req_headers = parse_request_headers(headers);
     // method and headers get logged by the instrument macro; this is just an example
     info!(
-        req.method = %method,
-        req.headers = ?parsed_req_headers,
+        request.endpoint = String::from(matched_path.as_str()),
+        request.method = %method,
+        request.headers = ?parsed_req_headers,
         "parsed request headers",
     );
     bytes
@@ -153,25 +159,26 @@ struct EchoJSONResponse {
     body: Value,
 }
 
-#[instrument(skip(headers, body), fields(req.headers.content_length = headers.len()))]
+#[instrument(skip(headers, body))]
 async fn echo_json(
     matched_path: MatchedPath,
     method: Method,
     headers: HeaderMap,
     Json(body): Json<Value>,
 ) -> Json<EchoJSONResponse> {
-    metrics::increment_counter!(
-        String::from(REQUEST_COUNT_METRIC_NAME),
-        METRIC_LABEL_SERVICE => SERVICE_NAME,
-        METRIC_LABEL_REQUEST_ENDPOINT => String::from(matched_path.as_str()),
-        METRIC_LABEL_REQUEST_METHOD => String::from(method.as_str()),
+    // ideally this would be in some middleware instead of manually in each handler
+    info!(
+        monotonic_counter.request_count = 1,
+        request.endpoint = String::from(matched_path.as_str()),
+        request.method = String::from(method.as_str()),
     );
     let req_method = method.to_string();
     let parsed_req_headers = parse_request_headers(headers);
     // method and headers get logged by the instrument macro; this is just an example
     info!(
-        req.method = req_method,
-        req.headers = ?parsed_req_headers,
+        request.endpoint = String::from(matched_path.as_str()),
+        request.method = req_method,
+        request.headers = ?parsed_req_headers,
         "parsed request headers",
     );
 
